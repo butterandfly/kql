8323df016ccfb6c2315973c21d1a0c6ae7f4baab
butterandfly <zero.hero.lin@gmail.com>
2022-06-02 11:49:32 +0800

    v0.0.1

---
+++ README.md
@@ -0,0 +1,4 @@
+# A Website for KQL
+
+## Development
+After cloning this project, `npm install` then `npm run dev`.
---
+++ src/routes/index.svelte
@@ -0,0 +1,56 @@
+
+<div class="container">
+  <div>
+  <h1>Kongqing's Lab</h1>
+  <a class="articles" href="#.">ARTICLES</a>
+  <a class="notes" href="#.">NOTES</a>
+  </div>
+</div>
+
+<script>
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Kalam:wght@400;700&display=swap');
+
+  h1 {
+    font-family: 'Kalam', cursive;
+    font-size: 64px;
+    color: #333;
+    margin: 0 0 32px;
+    text-shadow: 2px 2px 2px #ccc;
+  }
+
+  a {
+    font-family: 'Kalam', cursive;
+    font-size: 40px;
+    text-decoration: none;
+    display: block;
+    margin: 8px 0;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .articles {
+    color: teal;
+    text-shadow: 2px 2px 2px #ccc;
+  }
+
+  .notes {
+    color: steelblue;
+    text-shadow: 2px 2px 2px #ccc;
+  }
+
+  .container div {
+    text-align: center;
+  }
+
+  .container {
+    display: grid;
+    place-items: center;
+    height: calc(100vh - 16px);
+  }
+
+</style>
